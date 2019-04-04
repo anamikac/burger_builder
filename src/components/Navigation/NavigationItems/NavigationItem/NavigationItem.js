010fd75d0f4cafa46c5aafe9c2511ed188cd3939
Anamika Chandrakar <anamika.chandrakar.contractor@move.com>
2019-04-04 19:16:27 +0530

    Added SideDrawer

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,10 @@ import React from 'react';
 import classes from './NavigationItem.css'
 
 const navigationItem = (props) => (
-    <li></li>
+    <li className={classes.NavigationItem}>
+        <a className={props.active? classes.active: null} 
+        href={props.link}>{props.children}</a>
+    </li>
 );
 
 export default navigationItem;
